a441ccd830e8154ee1640aa3ca0c68d7e731e274
Elisa Bothy <elisa.bothy@gmail.com>
2024-02-29 14:17:34 +0100

    Création du menu principal, création du fichier code grâce à une fct(a fct va être supprimer à l'avenir), création du singleton mariadb. Il manque au menu la fonctionnalité permettant de faire apparaitre l'administration quand on écrit le bon code, il manque les fct d'accès et de modification de la data base pour l'onglet question

---
+++ src/main/java/graphics/AdministrationFrame.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphics;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Elisa Bothy
+ */
+public class AdministrationFrame extends JPanel{
+    
+}
--- src/main/java/graphics/Game.java
+++ src/main/java/graphics/Game.java
@@ -1,11 +1,106 @@
 package graphics;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 /**
  *
  * @author Herbert Caffarel
  */
 public class Game extends JFrame {
+    JPanel south;
+    JTabbedPane tabGraphique;
+    JTabbedPane calcul;
+    JTabbedPane question;
+    JTabbedPane administration;//pas visible tant que le code donné n'est pas le bon
+    JButton connexion;//pour se connecter à l'administratif
+    JPanel jpCalcul;
+    JPanel jpQuestion;
+    JPanel jpTabGraphique;
+    JPanel jpAdministration;
+    public Game(){
+        south = new JPanel();
+        jpCalcul = new JPanel();
+        jpQuestion = new JPanel();
+        jpTabGraphique = new JPanel();
+        jpAdministration = new JPanel();
+        tabGraphique = new JTabbedPane();
+        calcul = new JTabbedPane();
+        question = new JTabbedPane();
+        administration = new JTabbedPane();
+        connexion = new JButton("Connectez vous");
+        
+        //ajout des panels aux onglets 
+        tabGraphique.addTab("Ardoise magique", jpTabGraphique); 
+        calcul.addTab("Math'Adore", jpCalcul); 
+        question.addTab("Le Savez vous ?", jpQuestion);  
+        administration.addTab("Administration", jpAdministration); 
+        
+        creationCode();
+        initGui();
+        initEvents();
+        
+        //empêche d'agrandir la fenêtre
+        this.setResizable(false);
+        //agrandir la fenêtre avec ce qui la contient
+        this.pack();
+        //centrer fenêtre
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
 
+    private void initGui() {
+        jpTabGraphique.setPreferredSize(new Dimension(1300, 300));
+        jpCalcul.setPreferredSize(new Dimension(650, 200));
+        jpQuestion.setPreferredSize(new Dimension(650, 200));
+        jpAdministration.setPreferredSize(new Dimension(550, 100));
+         // disposition pour la JFrame principale
+        this.setLayout(new BorderLayout());
+
+        // Ajout des onglets à la JFrame principale
+        this.add(tabGraphique, BorderLayout.NORTH);
+        this.add(calcul, BorderLayout.WEST);
+        this.add(question, BorderLayout.EAST);
+        south.add(administration);
+        south.add(connexion);
+        this.add(south, BorderLayout.SOUTH);
+    }
+
+    private void initEvents() {
+        connexion.addActionListener((ActionEvent ae) -> {
+                String code = JOptionPane.showInputDialog(
+                null, 
+                "Entrez votre mot de passe", 
+                "Connexion", 
+                JOptionPane.QUESTION_MESSAGE                
+                );
+            });
+    }
+    //création du password dans un fichier
+    private void creationCode() {
+        DataOutputStream out;
+ 
+        try {
+            out = new DataOutputStream(
+                    new BufferedOutputStream(
+                        new FileOutputStream("code.dat")));
+            out.writeUTF("password");
+            out.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+            System.exit(1);
+        }
+    }
+    
 }
