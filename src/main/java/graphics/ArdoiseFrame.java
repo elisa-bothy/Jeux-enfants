0f6ce66194338a61a4392270ff1eb619c3057c20
Kévin H <hoppenotkevin@gmail.com>
2024-02-29 15:06:09 +0100

    third commit - Ardoise magique : dessiner ok, couleurs ok, bouton + icon effacer ok

--- src/main/java/graphics/ArdoiseFrame.java
+++ src/main/java/graphics/ArdoiseFrame.java
@@ -4,11 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -30,18 +32,26 @@ public class ArdoiseFrame extends JFrame {
         this.dessin = new JPanel();
         this.container = new JPanel();
         this.colorPanel = new JPanel();
-        this.clearButton = new JButton("Effacer");
+        this.clearButton = new JButton("");
+        //icone du bouton effacer
+        clearButton.setIcon(new ImageIcon("limpar-limpo.png"));
 
+        // Charger l'icône à partir d'un fichier
+        ImageIcon icon = new ImageIcon("dessin.png");
+
+        // Obtenir l'image de l'icône
+        Image image = icon.getImage();
+        
 
         dessin.setBackground(Color.white);
-        container.addMouseListener(new MouseAdapter() {
+        dessin.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 x = e.getX();
                 y = e.getY();
             }
         });
-        container.addMouseMotionListener(new MouseMotionAdapter() {
+        dessin.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
                 Graphics g = dessin.getGraphics();
@@ -54,7 +64,6 @@ public class ArdoiseFrame extends JFrame {
 
         container.setLayout(new BorderLayout());
         container.add(label, BorderLayout.SOUTH);
-        container.add(dessin, BorderLayout.CENTER);
 
         // Config Panel de couleur
         colorPanel.setLayout(new BoxLayout(colorPanel, BoxLayout.Y_AXIS));
@@ -66,7 +75,8 @@ public class ArdoiseFrame extends JFrame {
         colorPanel.add(createColorButton(Color.GRAY));
 
         container.add(colorPanel, BorderLayout.EAST);
-        container.add(clearButton, BorderLayout.SOUTH);
+        container.add(clearButton, BorderLayout.WEST);
+        container.add(dessin, BorderLayout.CENTER);
 
         // Ajoute un ActionListener au bouton Effacer
         clearButton.addActionListener((ActionEvent e) -> {
@@ -74,6 +84,8 @@ public class ArdoiseFrame extends JFrame {
         });
 
         this.setSize(1080, 720);
+        this.setIconImage(image);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setContentPane(container);
